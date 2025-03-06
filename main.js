743e2795717c03867075d0c202ed7f2163d7961c
Crimson-KING-38 <shreyasshaurya01@gmail.com>
2025-03-07 03:09:41 +0530

    v2.0.0
    Added a new variable.

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 let a = 25;
 let b = 50;
+let c = 125;
 
-let value = a + b;
+let value = a + b + c;
 
 console.log("a: ", a);
 console.log("b: ", b);
+console.log("b: ", c);
 console.log("value: ", value);
